d39e4045a61081e866fc4d549e42653781e2ce06
redditor8704 <redditor8704@gmail.com>
2024-03-05 19:19:57 +0100

    updated UI

--- src/components/tablesfromExcel/ExcelTablesActionBar.tsx
+++ src/components/tablesfromExcel/ExcelTablesActionBar.tsx
@@ -14,12 +14,12 @@ export default function ExcelTablesActionBar({ data, isLoading, isDisabled, hand
     return (
         <>
             {data.length > 0 ?
-                <HStack position={"fixed"} bottom={0} left={0} right={0} height={50} background={"#FFFFFF"} spacing={3} >
+                <HStack position={"fixed"} bottom={0} left={0} right={0} height={46} background={"#FFFFFF"} spacing={3} padding={2}>
                         <CustomButton onClick={handleTableCreation} isLoading={isLoading} isDisabled={isDisabled} w={(tableIndex < data.length - 1) ? "75%" : "100%"}> Create</CustomButton>
                         {
                             (tableIndex < data.length - 1) ?
                                 <>
-                                    <CustomButton onClick={() => setTableIndex(tableIndex + 1)}> Next</CustomButton>
+                                    <CustomButton onClick={() => setTableIndex(tableIndex + 1)} w={"25%"}> Next</CustomButton>
                                     <Text> {tableIndex + 1} of {data.length}</Text>
                                 </> : <></>
                         }
